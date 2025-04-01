0ad9fc8754e2523ab60961aded367028449e5c09
Skelly0 <skelly9912@gmail.com>
2025-04-01 16:32:10 +0100

    feat: Added Phi-4 as a contexualisation fallback

--- managers/documents.py
+++ managers/documents.py
@@ -376,6 +376,7 @@ class DocumentManager:
                 #"google/gemini-2.0-flash-exp:free",
                 #"google/gemma-3-27b-it:free",
                 "cohere/command-r7b-12-2024",
+                "microsoft/phi-4-multimodal-instruct",
                 "qwen/qwen-turbo",
                 "google/gemma-3-12b-it",
                 "google/gemini-2.0-flash-lite-001",
